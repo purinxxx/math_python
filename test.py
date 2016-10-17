de8dad721ed87078201a0cae98f3ded244b081e0
purinxxx <kengo@MacBook-Pro-5.local>
2016-10-17 16:19:08 +0900

    tex形式の繁分数の数式を出力

--- test.py
+++ test.py
@@ -7,5 +7,13 @@ def list_to_frac(l):
 		expr = 1/expr
 	return l[0] + expr
 
+def list_to_frac_expr(l):
+	expr = ''
+	for i in l:
+		expr += str(i) + '+1/('
+	expr = expr[:-4] + ')' * (len(l)-1)
+	return expr
+
 print list_to_frac([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+print list_to_frac_expr([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
 
